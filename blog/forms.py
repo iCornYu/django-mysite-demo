c9186c3586bd1e9c67e49288534a9769d32599e6
iCornYu <cookie3bar@gmail.com>
2021-08-12 19:25:12 -0700

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+#import models
+from .models import Post
+
+
+from django.contrib.auth.models import User
+#importing the django user form 
+from django.contrib.auth.forms import UserCreationForm
+
+class PostForm(forms.ModelForm):
+    class Meta: 
+        model = Post
+        fields = ['title', 'author', 'content']
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
---
+++ blog/views.py
@@ -0,0 +1,139 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+#import models (so we can query from database)
+from .models import Post
+
+#import forms ( so we can display it in our template)
+from .forms import PostForm, UserForm
+
+#import extra functionality: (authentication/login/logout/messages)
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+#import mail dependencies
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+
+# Create your views here.
+def index(request):
+    my_posts = [
+        {
+            'author' : 'Shoha Tsuchida',
+            'title': 'This is my first post!',
+            'content' : 'content1',
+            'date_posted' : 'August 9, 2021'
+        },
+        {
+            'author' : 'Ivan Wang',
+            'title': "This is Ivan's first post!",
+            'content' : 'content from Ivan',
+            'date_posted' : 'August 9, 2021'
+        }
+    ]
+    context = {'posts': my_posts}
+    # <!-- Syntax for adding python vars: {{ var_goes here}}-->
+    # <!-- Syntax for adding python vars: {% expression_goes_here %}-->
+    return render(request, 'blog/index.html', context)
+
+def aboutPage(request):
+    return render(request, 'blog/about.html')
+
+def posts(request):
+    posts = Post.objects.order_by('id').all()
+    context = {
+        'posts': posts,
+        'page_title' : 'Welcome to my blog'
+    }
+    return render(request, 'blog/posts.html', context)
+
+@login_required(login_url='blog-login')
+def createPost(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid(): 
+        form.save() 
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/createpost.html', context)
+
+def registerPage(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        print('form submitted')
+        form.save()
+
+
+        user = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+
+        context = {"username" : user}
+
+        template = render_to_string('blog/emailtemplate.html', context)
+
+        email_message = EmailMessage(
+            'Welcome to my Django Blog!', #Subject Line
+            template, #body
+            settings.EMAIL_HOST_USER, #host_email
+            [email], #recipient
+        )
+
+        email_message.fail_silently = False 
+        email_message.send() 
+
+        messages.success(request, "Account was created for " + user)
+
+        return redirect('blog-login')
+    context = { 'form': form}
+    return render(request, 'blog/register.html', context)
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username") #comes from name attribute in html input tag
+        password = request.POST.get("password1")
+        print(username,password)
+        user= authenticate(request, username = username, password = password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            print(f'{user} is logged in.')
+            return redirect('blog-index')
+        messages.info(request, "Incorrect username or password. Please try again.")
+    return render(request,'blog/login.html')
+
+@login_required(login_url = 'blog-login')
+def logoutUser(request):
+    logout(request)
+    return redirect('blog-login')
+
+def individualPost(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context ={'p':post}
+    return render(request, 'blog/individualpost.html', context)
+
+@login_required(login_url = 'blog-login')
+def updatePost(request,post_id):
+    post = Post.objects.get(id=post_id)
+    form = PostForm(instance=post)
+    if post.author.id != request.user.id and not request.user.is_superuser:
+            messages.info(request, "You can not update another user's post!")
+            return redirect('blog-posts')
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "You have successfully updated your post.")
+        return redirect('blog-individualpost', post_id=post_id)
+    context = {'form': form}
+    return render(request, 'blog/updatepost.html', context)
+
+@login_required(login_url = 'blog-login')
+def deletePost(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if post.author.id != request.user.id:
+        messages.info(request, "You can not delete another user's post!")
+        return redirect('blog-posts')
+    post.delete() 
+    messages.success(request, "The post has been successfully deleted.")
+    return redirect('blog-posts')
